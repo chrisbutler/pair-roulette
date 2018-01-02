c5728bb300bde311ecc7d34658b881a2e635d9ae
Chris Butler <chris@platformthirteen.com>
2018-01-01 19:56:14 -0500

    use env port

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const slack = ts.instance({});
 
 slack.on('/pairis', payload => {
   const { user_id, response_url, channel_id } = payload;
-  
+
   const list = payload.text.split(' ')
   const pairOptions = _.chain(list).shuffle().chunk(2).value();
-  
+
   const pairsText = _.map(pairOptions, pair => {
     return pair[1] ? `*${_.capitalize(pair[0])}* pairs with *${_.capitalize(pair[1])}*` : `*${_.capitalize(pair[0])}* is unpaired`;
   });
-  
-  const message = { 
+
+  const message = {
     response_type: 'in_channel',
     channel: channel_id,
     text: `Creating pairs from: ${payload.text}`,
@@ -22,9 +22,9 @@ slack.on('/pairis', payload => {
       { text:  pairsText.join('\n'), 'mrkdwn_in': ['text'] }
     ]
   };
-  
+
   console.log('Received /pairis command', payload, pairOptions, pairsText, message);
-  
+
   slack.send(response_url, message).then(res => {
     console.log('Response sent to slash command');
   }, error => {
@@ -32,4 +32,4 @@ slack.on('/pairis', payload => {
   });
 });
 
-slack.listen('3000');
+slack.listen(process.env.PORT);
